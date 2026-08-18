857421200fd70245e0fe1a34e8ec3e10d24a001d
yuxrmikey-workspace <yuxrmikey@gmail.com>
2026-08-18 17:26:35 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}", // C'est cette ligne qui dit à Tailwind de lire vos fichiers JS
   ],
   theme: {
     extend: {},
